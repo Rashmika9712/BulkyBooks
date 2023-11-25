77cf5b6f0d6aef8fb0b357a1c63b0b1f2ceee421
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-11-25 17:21:00 +0530

    Add ApplicationDbContext

---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using src.Models;
+
+namespace src.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
