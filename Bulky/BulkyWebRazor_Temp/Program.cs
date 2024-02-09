f79ffdafd7a999f60da2a464e4ee9bc8cb0438f2
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2024-02-09 23:06:43 +0530

    conections

--- Bulky/BulkyWebRazor_Temp/Program.cs
+++ Bulky/BulkyWebRazor_Temp/Program.cs
@@ -1,8 +1,14 @@
+using BulkyWebRazor_Temp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultString"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
