586a992f67110c10d73d2e7b3d832c0fd6b6c50d
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2024-02-12 23:20:05 +0530

    Implement Product

--- Bulky/Bulky.DataAccess/Migrations/20240212164734_add_Product_Db.Designer.cs
+++ Bulky/Bulky.DataAccess/Migrations/20240212164734_add_Product_Db.Designer.cs
@@ -2,15 +2,18 @@
 using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Bulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240212164734_add_Product_Db")]
+    partial class add_Product_Db
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bulky/BulkyBooks/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,106 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBooks.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> obj = _unitOfWork.Product.GetAll().OrderBy(x => x.Title).ToList();
+            return View(obj);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["error"] = "Product does not exists!";
+                return NotFound();
+            }
+
+            Product? product = _unitOfWork.Product.Get(x => x.Id == id);
+
+            if (id == null)
+            {
+                TempData["error"] = "Product does not exists!";
+                return NotFound();
+            }
+
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["error"] = "Product does not exists!";
+                return NotFound();
+            }
+
+            Product? product = _unitOfWork.Product.Get(x => x.Id == id);
+
+            if (id == null)
+            {
+                TempData["error"] = "Product does not exists!";
+                return NotFound();
+            }
+
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? product = _unitOfWork.Product.Get(x => x.Id == id);
+
+            if (product == null)
+            {
+                TempData["error"] = "Product does not exists!";
+                return NotFound();
+            }
+
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfully!";
+            return RedirectToAction("Index");
+        }
+    }
+}
