e20e79cbc643c5df29c6b24d934cbf9a7e01c9fe
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2024-09-16 20:04:24 +0530

    Include Repo

--- Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -4,8 +4,8 @@ namespace Bulky.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        T Get(Expression<Func<T, bool>> filter);
-        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        IEnumerable<T> GetAll(string? includeProperties = null);
         void Remove(T entity);
         void Add(T entity);
         void RemoveRange(IEnumerable<T> entity);
